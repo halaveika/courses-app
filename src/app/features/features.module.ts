405dde4bd1a01816815d25f10891acc3c6c95a60
Aliaksandr <halaveika1988@gmail.com>
2023-02-14 00:15:03 +0100

    feat: add boilerplate component login and registration

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { CoursesComponent } from './courses/courses.component';
 import { CourseComponent } from './course/course.component';
 import {SharedModule} from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 
 @NgModule({
   declarations: [
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     CommonModule,
