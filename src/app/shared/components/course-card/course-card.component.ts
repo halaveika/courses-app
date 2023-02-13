af56d0136feb8941eb0e12dc9988941d35172b81
Aliaksandr <halaveika1988@gmail.com>
2023-02-13 22:36:24 +0100

    feat: add course card component

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.scss']
 })
-export class CourseCardComponent {}
+export class CourseCardComponent {
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() creationDate: Date = new Date();
+  @Input() duration: number = 0;
+  @Input() authors: string[] = [];
+  @Input() editable: boolean = true;
+
+  formatDuration(duration: number): string {
+    const hours = Math.floor(duration / 60);
+    const minutes = duration % 60;
+    return `${hours}:${minutes.toString().padStart(2, '0')} hours`;
+  }
+}
