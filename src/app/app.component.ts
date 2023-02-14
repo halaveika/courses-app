471ce0793f75b0f89a2d3beea83cb6e13b3396c7
Aliaksandr <halaveika1988@gmail.com>
2023-02-14 16:27:41 +0100

    add info section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,45 @@
 import { Component, Input } from '@angular/core';
 
+type InfoData = {
+  title: string,
+  text: string
+}
+
+const emptyInfo = {
+  title:'Your List Is Empty',
+  text:'Please use ’Add New Course’ button to add your first course'
+}
+
+const mockInfo = {
+  title:'angular-course',
+  text:''
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'courses-app';
-  @Input() user: string = '';
-  @Input() isLogged: boolean = false;
+  title = emptyInfo.title;
+  text = emptyInfo.text;
+  user: string = '';
+  isLogged: boolean = false;
 
   loginLogout() {
     if(this.isLogged) {
       this.isLogged = false;
       this.user = '';
+      this.setInfo(emptyInfo)
       return;
     }
       this.isLogged = true;
       this.user = 'user';
+      this.setInfo(mockInfo)
+  }
+
+  setInfo(infoData:InfoData) {
+    this.title = infoData.title;
+    this.text = infoData.text;
   }
 }
